cf20fa002bce9cb3b6e6beeebc44489f2279cde9
tomyshogo <96429804+tomyshogo@users.noreply.github.com>
2024-03-12 20:37:59 +0900

    初回コミット
    初回

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import TopPage  from './pages/TopPage';
+
+function App() {
+  return (
+    <div className="App">
+      <TopPage/>
+    </div>
+  );
+};
+
+export default App;
